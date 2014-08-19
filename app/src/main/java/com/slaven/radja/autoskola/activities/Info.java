75975f4ff1bcc48d6f8fe7d5f56b8aa2454933d2
tito6666 <sradja2@gmail.com>
2014-08-19 19:43:57 +0200

    adas

--- app/src/main/java/com/slaven/radja/autoskola/activities/Info.java
+++ app/src/main/java/com/slaven/radja/autoskola/activities/Info.java
@@ -1,6 +1,7 @@
 package com.slaven.radja.autoskola.activities;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.slaven.radja.autoskola.R;
 
@@ -8,11 +9,23 @@ import com.slaven.radja.autoskola.R;
  * Created by Computer on 04/08/2014.
  */
 public class Info extends BaseActivity {
+
+    TextView tvInfo;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.info);
         rootView = findViewById(R.id.root_view);
         setBackground();
+
+        tvInfo = (TextView) findViewById(R.id.tvInfo);
+
+        tvInfo.setText("\n Ova aplikacija je namijenjena testiranju znanja koje je potrebno kako bi se položio pismeni ispit" +
+                " za autoškolu u Republici Hrvatskoj.\n\n"
+                        + "U nju je implemenitrana baza podataka koja sadrži neke od podataka koji se pojavljuju na službenim testovima.\n\n"
+                        + "Napomena:\n\n"
+                        + "- aplikacija služi isključivo kao dodatni alat pri učenju, nikako kao i jedini izvor infromacija");
+
     }
 }
--- app/src/main/java/com/slaven/radja/autoskola/activities/ucenje/KruzniTokRotor.java
+++ app/src/main/java/com/slaven/radja/autoskola/activities/ucenje/KruzniTokRotor.java
@@ -1,6 +1,8 @@
 package com.slaven.radja.autoskola.activities.ucenje;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.slaven.radja.autoskola.R;
 import com.slaven.radja.autoskola.activities.BaseActivity;
@@ -9,11 +11,25 @@ import com.slaven.radja.autoskola.activities.BaseActivity;
  * Created by Computer on 05/08/2014.
  */
 public class KruzniTokRotor extends BaseActivity {
+
+    ImageView ivKruzniTok;
+    TextView tvKruzniTok;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.kruzni_tok_rotor);
         rootView = findViewById(R.id.root_view);
         setBackground();
+
+        ivKruzniTok = (ImageView) findViewById(R.id.ivKruzniTok);
+        tvKruzniTok = (TextView) findViewById(R.id.tvKruzniTok);
+
+
+        tvKruzniTok.setText("\n Službeni naziv za kružni tok - rotor je prometno raskrižje sa kružnim tokom prometa.\n\n"
+        + "- U kružnom toku imaju prednost vozila koja zadržavaju smjer ravno i svoju prometnu traku \n\n"
+        + "- Prilikom ulaska u kružni tok ne dajemo žmigavac. To je prometno ravno\n\n"
+        + "- Prije izlaska iz kružnog toka dajemo desni žmigavac. Na taj način obavještavamo vozila da izlazimo iz kružnog toka i olakšavamo njihov ulazak\n");
+
     }
 }
